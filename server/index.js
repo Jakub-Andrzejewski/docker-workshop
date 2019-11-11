51b0ed4d571887344731384374b05d0f5539302c
Baroshem <jakub.andrzejewski.dev@gmail.com>
2019-11-11 20:43:30 +0100

    Changed port on server and added Dockerfile to client

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ app.get('/', (req, res) => {
 res.send("Hello from Express.js");
 });
 
-app.listen(3000);
-console.log("Listening on port 3000");
+app.listen(3001);
+console.log("Listening on port 3001");
